96ea658c1ac5e5305f34bdb24c28a75ad32d4fa5
mattbatwings <mfpalazzolo@gmail.com>
2023-11-30 18:57:52 -0500

    single part 2

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ def main():
     sports = ['baseball', 'basketball', 'football', 'soccer']
         
     # Part 2
-    for i in range(50):
-        gather_and_store.populate_baseball_table()
-        gather_and_store.populate_basketball_table()
-        gather_and_store.populate_football_table()
-        gather_and_store.populate_soccer_table()
+    gather_and_store.populate_baseball_table()
+    gather_and_store.populate_basketball_table()
+    gather_and_store.populate_football_table()
+    gather_and_store.populate_soccer_table()
     
     # Part 3
     for sport in sports:
